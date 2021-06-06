dd22598ae4dc8ea5be9b65d992c204e315d1ddec
Bing-su <ks2515@naver.com>
2021-06-06 19:55:14 +0900

    try 3

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
-FROM python:3.8
+FROM python:3.8-slim
 
 WORKDIR /app
 
-COPY . /app
+COPY requirements.txt ./requirements.txt
 
 RUN pip3 install -r requirements.txt
+
+COPY . /app
 
 EXPOSE 8501
 
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 
-option = st.selectbox("Select one", ["Hello, World!", "foo", "ham"])
-d = {"Hello, World!": "Hello, World!", "foo": "bar", "ham": "spam"}
+option = st.selectbox("Select one", ["Hello, World!", "foo", "spam"])
+d = {"Hello, World!": "Hello, World!", "foo": "bar", "spam": "ham"}
 
 st.title(d[option])
