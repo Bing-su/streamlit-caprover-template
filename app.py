0f883e97f01efe045f01213803c2c4bd62e1f246
Bing-su <ks2515@naver.com>
2021-06-07 10:01:51 +0900

    test

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 
-option = st.selectbox("Select one", ["Hello, World!", "foo", "spam"])
-d = {"Hello, World!": "Hello, World!", "foo": "bar", "spam": "ham"}
+option = st.selectbox("Select one", ["Hello, World!", "foo", "spam", "python"])
+d = {"Hello, World!": "Hello, World!", "foo": "bar", "spam": "ham", "python": "3.8.10"}
 
 st.title(d[option])
